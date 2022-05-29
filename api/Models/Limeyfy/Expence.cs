803435f809b3bc16939fcc3a5986097a2c2a33ac
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-05-29 12:47:03 +0200

    Expence model

---
+++ api/Models/Limeyfy/Expence.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Limeyfy.API.Models.Limeyfy;
+
+public class Expence
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string Id { get; set; }
+    
+    public string UserId { get; set; }
+    
+    public DateTime Date { get; set; }
+    
+    public double Amount { get; set; }
+    
+    public string Description { get; set; }
+    
+    public string Category { get; set; }
+    
+    public string To { get; set; }
+    
+    public string Why { get; set; }
+}
