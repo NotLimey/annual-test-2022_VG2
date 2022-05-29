ffa376f71a63bda0f65960a9ae0a31eb00e91968
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-05-29 11:09:32 +0200

    Hide data on sensitive data mode

--- frontend/src/components/statistics/RevenueStatistics.tsx
+++ frontend/src/components/statistics/RevenueStatistics.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "react-query";
 import { fetchStatisticsRevenue, fetchStatisticsRevenueLastYear } from "../../scripts/fetch";
 import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { useEffect, useState } from "react";
+import { classNames } from "../../scripts/tailwind";
+import useSettings from "../../hooks/useSettings";
 
 interface IRevenueStatisticsData {
     lastYear: number;
@@ -17,6 +19,7 @@ interface IRevenueStatisticsData {
 const RevenueStatistics = () => {
     const { data } = useQuery("statistics_revenue_last_year", fetchStatisticsRevenue);
     const [stats, setStats] = useState<IRevenueStatisticsData>();
+    const { settings } = useSettings();
 
     useEffect(() => {
         if (data?.data) {
@@ -27,40 +30,41 @@ const RevenueStatistics = () => {
     if (!stats) return <></>;
 
     return (
-        <div className="mt-5">
-            <div>
-                <h3 className="text-lg md:text-xl leading-6 font-medium text-gray-900 dark:text-gray-100">Revenue over the last year</h3>
-                <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-                    <div className="px-4 py-5 bg-white dark:bg-stone-800 shadow rounded-lg overflow-hidden sm:p-6">
-                        <dt className="text-sm font-medium text-gray-500 dark:text-gray-300 truncate">Last 30 days</dt>
-                        <dd className="mt-1 text-3xl font-semibold text-gray-900 dark:text-gray-100">Kr {stats.last30Days.toLocaleString()},- </dd>
-                    </div>
-                    <div className="px-4 py-5 bg-white dark:bg-stone-800 shadow rounded-lg overflow-hidden sm:p-6">
-                        <dt className="text-sm font-medium text-gray-500 dark:text-gray-300 truncate">Last 12 months</dt>
-                        <dd className="mt-1 text-3xl font-semibold text-gray-900 dark:text-gray-100">Kr {stats.thisYear.toLocaleString()},- </dd>
-                    </div>
-                    <div className="px-4 py-5 bg-white dark:bg-stone-800 shadow rounded-lg overflow-hidden sm:p-6">
-                        <dt className="text-sm font-medium text-gray-500 dark:text-gray-300 truncate">Total</dt>
-                        <dd className="mt-1 text-3xl font-semibold text-gray-900 dark:text-gray-100">Kr {stats.total.toLocaleString()},- </dd>
-                    </div>
-                </dl>
-            </div>
-            <div className="w-full h-96 bg-gray-200 px-5 py-3 rounded-lg shadow-md dark:bg-stone-800 my-5 flex flex-col">
-                <div className="w-full h-full max-h-fit">
-
-                    <ResponsiveContainer width="100%" height="100%">
-                        <ResponsiveContainer width="100%" height="100%">
-                            <LineChart width={300} height={100} data={stats.thisYearDataSets}>
-                                <Tooltip />
-                                <YAxis />
-                                <XAxis dataKey="label" />
-                                <Line type="monotone" dataKey="value" stroke="#5cb85c" strokeWidth={2} unit=" Kr" name="Revenue" />
-                            </LineChart>
-                        </ResponsiveContainer>
-                    </ResponsiveContainer>
+        <>
+            <div className={classNames(settings.sensitiveDataMode ? "blur-lg" : "", "mt - 5")}>
+                <div>
+                    <h3 className="text-lg md:text-xl leading-6 font-medium text-gray-900 dark:text-gray-100">Revenue over the last year</h3>
+                    <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+                        <div className="px-4 py-5 bg-white dark:bg-stone-800 shadow rounded-lg overflow-hidden sm:p-6">
+                            <dt className="text-sm font-medium text-gray-500 dark:text-gray-300 truncate">Last 30 days</dt>
+                            <dd className="mt-1 text-3xl font-semibold text-gray-900 dark:text-gray-100">Kr {stats.last30Days.toLocaleString()},- </dd>
+                        </div>
+                        <div className="px-4 py-5 bg-white dark:bg-stone-800 shadow rounded-lg overflow-hidden sm:p-6">
+                            <dt className="text-sm font-medium text-gray-500 dark:text-gray-300 truncate">Last 12 months</dt>
+                            <dd className="mt-1 text-3xl font-semibold text-gray-900 dark:text-gray-100">Kr {stats.thisYear.toLocaleString()},- </dd>
+                        </div>
+                        <div className="px-4 py-5 bg-white dark:bg-stone-800 shadow rounded-lg overflow-hidden sm:p-6">
+                            <dt className="text-sm font-medium text-gray-500 dark:text-gray-300 truncate">Total</dt>
+                            <dd className="mt-1 text-3xl font-semibold text-gray-900 dark:text-gray-100">Kr {stats.total.toLocaleString()},- </dd>
+                        </div>
+                    </dl>
                 </div>
-            </div>
-        </div>
+                <div className={classNames(settings.sensitiveDataMode ? "hidden" : "", "w-full h-96 bg-gray-200 px-5 py-3 rounded-lg shadow-md dark:bg-stone-800 my-5 flex flex-col")}>
+                    <div className="w-full h-full max-h-fit">
+                        <ResponsiveContainer width="100%" height="100%">
+                            <ResponsiveContainer width="100%" height="100%">
+                                <LineChart width={300} height={100} data={stats.thisYearDataSets}>
+                                    <Tooltip />
+                                    <YAxis />
+                                    <XAxis dataKey="label" />
+                                    <Line type="monotone" dataKey="value" stroke="#5cb85c" strokeWidth={2} unit=" Kr" name="Revenue" />
+                                </LineChart>
+                            </ResponsiveContainer>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
+            </div >
+        </>
     );
 }
 
