72d5459e9180916e881b20ebe8244700c044e6f7
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-05-30 09:11:12 +0200

    Description list

--- frontend/src/components/information/DescriptionList.tsx
+++ frontend/src/components/information/DescriptionList.tsx
@@ -1,65 +1,52 @@
-import { PaperClipIcon } from "@heroicons/react/outline";
+import { formatPascalAndSpace } from "@/scripts/text";
+import { DuplicateIcon, PaperClipIcon } from "@heroicons/react/outline";
 
-const DescriptionList = () => {
+interface DescriptionListProps {
+    object?: any;
+    title?: string;
+    ignoreValues?: string[];
+    customFormatting?: DescriptionListCustomField[];
+}
+
+interface DescriptionListCustomField {
+    name: string;
+    func: (value: any) => string | React.ReactNode;
+}
+
+const DescriptionList = ({ object, title, ignoreValues, customFormatting }: DescriptionListProps) => {
     return (
-        <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+        <div className="bg-white dark:bg-stone-800 shadow overflow-hidden sm:rounded-lg">
             <div className="px-4 py-5 sm:px-6">
-                <h3 className="text-lg leading-6 font-medium text-gray-900">Applicant Information</h3>
-                <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+                <h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">{title ?? "Applicant Information"}</h3>
+                <p className="mt-1 max-w-2xl text-sm text-gray-500 dark:text-gray-400">Personal details and application.</p>
             </div>
-            <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+            <div className="border-t border-gray-200 dark:border-stone-700 px-4 py-5 sm:px-6">
                 <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
-                    <div className="sm:col-span-1">
-                        <dt className="text-sm font-medium text-gray-500">Full name</dt>
-                        <dd className="mt-1 text-sm text-gray-900">Margot Foster</dd>
-                    </div>
-                    <div className="sm:col-span-1">
-                        <dt className="text-sm font-medium text-gray-500">Application for</dt>
-                        <dd className="mt-1 text-sm text-gray-900">Backend Developer</dd>
-                    </div>
-                    <div className="sm:col-span-1">
-                        <dt className="text-sm font-medium text-gray-500">Email address</dt>
-                        <dd className="mt-1 text-sm text-gray-900">margotfoster@example.com</dd>
-                    </div>
-                    <div className="sm:col-span-1">
-                        <dt className="text-sm font-medium text-gray-500">Salary expectation</dt>
-                        <dd className="mt-1 text-sm text-gray-900">$120,000</dd>
-                    </div>
+                    {Object.entries(object).map((value, idx) => {
+                        if (ignoreValues?.includes(value[0])) return <></>
+
+                        const _customFormatting = customFormatting?.find(x => x.name === value[0]);
+
+                        if (_customFormatting) return (
+                            <div key={idx} className="sm:col-span-1">
+                                <dt className="text-sm font-medium text-gray-500 dark:text-gray-400 flex">{formatPascalAndSpace(value[0])}  <DuplicateIcon className="h-4 w-4 ml-1 cursor-pointer" /></dt>
+                                <dd className="mt-1 text-sm text-gray-900 dark:text-gray-100">{_customFormatting.func(value[1])}</dd>
+                            </div>
+                        )
+
+                        return (
+                            <div key={idx} className="sm:col-span-1">
+                                <dt className="text-sm font-medium text-gray-500 dark:text-gray-400 flex">{formatPascalAndSpace(value[0])}  <DuplicateIcon className="h-4 w-4 ml-1 cursor-pointer" /></dt>
+                                <dd className="mt-1 text-sm text-gray-900 dark:text-gray-100">{(value[1] as any)}</dd>
+                            </div>
+                        )
+                    })}
                     <div className="sm:col-span-2">
-                        <dt className="text-sm font-medium text-gray-500">About</dt>
-                        <dd className="mt-1 text-sm text-gray-900">
+                        <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">About</dt>
+                        <dd className="mt-1 text-sm text-gray-900 dark:text-gray-100">
                             Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
                             qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
                             pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
-                        </dd>
-                    </div>
-                    <div className="sm:col-span-2">
-                        <dt className="text-sm font-medium text-gray-500">Attachments</dt>
-                        <dd className="mt-1 text-sm text-gray-900">
-                            <ul role="list" className="border border-gray-200 rounded-md divide-y divide-gray-200">
-                                <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                                    <div className="w-0 flex-1 flex items-center">
-                                        <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                        <span className="ml-2 flex-1 w-0 truncate">resume_back_end_developer.pdf</span>
-                                    </div>
-                                    <div className="ml-4 flex-shrink-0">
-                                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                            Download
-                                        </a>
-                                    </div>
-                                </li>
-                                <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                                    <div className="w-0 flex-1 flex items-center">
-                                        <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                        <span className="ml-2 flex-1 w-0 truncate">coverletter_back_end_developer.pdf</span>
-                                    </div>
-                                    <div className="ml-4 flex-shrink-0">
-                                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                            Download
-                                        </a>
-                                    </div>
-                                </li>
-                            </ul>
                         </dd>
                     </div>
                 </dl>
--- frontend/src/pages/auth/company/Company.tsx
+++ frontend/src/pages/auth/company/Company.tsx
@@ -1,17 +1,52 @@
+import { TCompany } from "@/types/Company";
 import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
-import Cms from "../../../components/cms/Cms";
 import DescriptionList from "../../../components/information/DescriptionList";
 import Loader from "../../../components/loaders/Loader";
 import useSettings from "../../../hooks/useSettings";
 import { fetchCompanies } from "../../../scripts/fetch";
-import { TCompany } from "@/types/Company";
 
 const Company = () => {
+    const { id } = useParams();
+    const [company, setCompany] = useState<null | TCompany>()
+    const { data, isLoading: dataLoading, refetch } = useQuery("auth_companies", fetchCompanies)
+
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const { settings } = useSettings();
+
+    useEffect(() => {
+        if (dataLoading || !id || !data) return;
+        const obj: TCompany = data.data.find((x: TCompany) => x.id === id);
+        if (obj) {
+            setCompany(obj);
+        } else {
+            setError(true);
+        }
+
+        setIsLoading(false)
+    }, [id, data, isLoading])
+
+    if (isLoading) return <Loader className="mt-12" />
+
+    if (error || !company) return (
+        <div>
+            Cannot find project with this id
+        </div>
+    )
+
     return (
         <DescriptionList
-
+            title={company.name}
+            object={company}
+            ignoreValues={["id", "name"]}
+            customFormatting={[
+                {
+                    name: "created",
+                    func: (val: string) => new Date(`${val}`).toLocaleDateString()
+                }
+            ]}
         />
     )
 }
